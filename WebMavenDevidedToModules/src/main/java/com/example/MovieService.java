4e5907a6eb90a6b7fd259f0910fd70180e49064b
maXo <89421851+maXoXx0@users.noreply.github.com>
2024-05-13 22:28:58 +0200

    Update MovieService.java

--- WebMavenDevidedToModules/src/main/java/com/example/MovieService.java
+++ WebMavenDevidedToModules/src/main/java/com/example/MovieService.java
@@ -11,7 +11,7 @@ public class MovieService {
 
 //      Creating URL
         String url =
-                "https://api.themoviedb.org/3/search/multi?api_key=[INSERT_HERE_YOUR_API_KEY]=" + searchContent;
+                "https://api.themoviedb.org/3/search/multi?api_key=[INSERT_YOUR_API_KEY]=" + searchContent;
         URL object = new URL(url);
 
 //      Creating connection
